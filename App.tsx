0aa8adcb5c7dca8a300cd6efdb78ff1b2b89a866
desteban <esteban28c@hotmail.com>
2020-04-06 15:10:38 -0500

    implemantanfo navegacion

--- App.tsx
+++ App.tsx
@@ -1,43 +1,54 @@
-import React from "react";
-import { StyleSheet, Text, View, StatusBar, Image } from "react-native";
-import { NativeRouter, Route, Link } from "react-router-native";
-import { SvgUri } from "react-native-svg";
+import * as React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 
-import Home from "./containers/Home";
-import Grupo from "./containers/Grupo";
-import Login from "./containers/Login";
-import Header from "./components/Header";
+import Login from './containers/Login';
+import Home from './containers/Home';
+import Grupo from './containers/Grupo';
 
-export default function App() {
-  return (
-    <NativeRouter>
-      <View style={styles.container}>
-        <Header />
-        <Route exact path="/" component={Login} />
-        <Route path="/grupo" component={Grupo} />
-        <Route path="/lista" component={Home} />
-      </View>
-    </NativeRouter>
-  );
+const Stack = createStackNavigator();
+const nombreApp = 'ChatUnicundi';
+
+function App() {
+	return (
+		<NavigationContainer>
+			<Stack.Navigator initialRouteName="Home">
+				<Stack.Screen
+					name="Home"
+					component={Home}
+					options={{ title: nombreApp, headerTitleAlign: 'center' }}
+				/>
+				<Stack.Screen name="Chat" component={Grupo} />
+				<Stack.Screen
+					name="Login"
+					component={Login}
+					options={{ headerTitleAlign: 'center', title: nombreApp }}
+				/>
+			</Stack.Navigator>
+		</NavigationContainer>
+	);
 }
 
+export default App;
+
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#F9F9F9"
-  },
-  title: {
-    fontSize: 20
-  },
-  header: {
-    flexDirection: "row",
-    textAlignVertical: "center",
-    padding: 3
-  },
-  img: {
-    height: 25,
-    width: 25,
-    margin: 3,
-    marginVertical: 2
-  }
+	container: {
+		flex: 1,
+		backgroundColor: '#F9F9F9'
+	},
+	title: {
+		fontSize: 20
+	},
+	header: {
+		flexDirection: 'row',
+		textAlignVertical: 'center',
+		padding: 3
+	},
+	img: {
+		height: 25,
+		width: 25,
+		margin: 3,
+		marginVertical: 2
+	}
 });
--- containers/Login.tsx
+++ containers/Login.tsx
@@ -1,43 +1,49 @@
-import React from "react";
-import { View, Button, StyleSheet, TextInput } from "react-native";
+import React from 'react';
+import { View, Button, StyleSheet, TextInput } from 'react-native';
 
 export default class Login extends React.Component {
-  render() {
-    return (
-      <View style={estilos.container}>
-        <TextInput
-          placeholder="Usuario(Aulas virtuales)"
-          style={estilos.inputs}
-        />
-        <TextInput
-          placeholder="Contraseña"
-          textContentType="password"
-          style={estilos.inputs}
-        />
-        <Button title="Ingresar" onPress={() => {}} />
-      </View>
-    );
-  }
+	constructor(props) {
+		super(props);
+		this.state = { navigation: props.navigation };
+	}
+
+	render() {
+		const toHome = () => {
+			this.state.navigation.push('Home');
+		};
+
+		return (
+			<View style={estilos.container}>
+				<TextInput placeholder="Usuario(Aulas virtuales)" style={estilos.inputs} />
+				<TextInput
+					placeholder="Contraseña"
+					textContentType="password"
+					style={estilos.inputs}
+				/>
+				<Button title="Ingresar" onPress={toHome} />
+			</View>
+		);
+	}
 }
 
 const estilos = StyleSheet.create({
-  btn: {
-    backgroundColor: "#26FF67"
-  },
-  container: {
-    flex: 1,
-    flexDirection: "column",
-    justifyContent: "center",
-    backgroundColor: "#F9F9F9",
-    padding: 5
-  },
-  inputs: {
-    borderRadius: 30,
-    borderColor: "#000000",
-    borderWidth: 2,
-    textAlign: "center",
-    height: 30,
-    fontSize: 20,
-    marginBottom: 15
-  }
+	btn: {
+		backgroundColor: '#26FF67'
+	},
+	container: {
+		flex: 1,
+		flexDirection: 'column',
+		justifyContent: 'center',
+		backgroundColor: '#F9F9F9',
+		padding: 5
+	},
+	inputs: {
+		borderRadius: 30,
+		borderColor: '#000000',
+		borderWidth: 2,
+		textAlign: 'center',
+		height: 30,
+		fontSize: 20,
+		marginBottom: 15
+	}
 });
