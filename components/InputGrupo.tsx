421312e622f196cbd2d0fb272b869420247f4914
desteban <esteban28c@hotmail.com>
2020-04-23 12:34:11 -0500

    coponente para el input del chat

---
+++ components/InputGrupo.tsx
@@ -0,0 +1,115 @@
+import * as React from 'react';
+import { View, TouchableOpacity, StyleSheet, TextInput } from 'react-native';
+import * as DocumentPicker from 'expo-document-picker';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+
+interface Iprops {
+	onChangeText?;
+	onContentSizeChange?;
+	value?;
+	send?;
+	height;
+}
+this;
+
+export default function InputGrupo(props: Iprops) {
+	return (
+		<View style={[estilos.inputContainer, { height: Math.max(45, props.height) }]}>
+			<View style={estilos.opciones}>
+				<TextInput
+					placeholder="Escribe un mensaje"
+					multiline={true}
+					onChangeText={(text) => props.onChangeText(text)}
+					onContentSizeChange={(event) => props.onContentSizeChange(event)}
+					style={[estilos.inputChat, { height: Math.max(45, props.height) }]}
+					value={props.value}
+				/>
+			</View>
+
+			<View style={{ flexDirection: 'column-reverse' }}>
+				<View style={estilos.iconBox}>
+					{/**
+					 * Icon add
+					 */}
+
+					<View style={estilos.iconContainer}>
+						<TouchableOpacity
+							style={estilos.icon}
+							onPress={() => {
+								DocumentPicker.getDocumentAsync({
+									type: '*/*',
+									copyToCacheDirectory: false
+								})
+									.then((document) => {
+										alert(`uri: ${document.uri}
+                                nombre: ${document.name}`);
+									})
+									.catch(() => alert('Algo salio mal'));
+							}}
+						>
+							<Icon name="add" size={30} color="white" />
+						</TouchableOpacity>
+					</View>
+
+					{/**
+					 * Icons send
+					 */}
+
+					<View style={estilos.iconContainer}>
+						<TouchableOpacity
+							style={[estilos.icon]}
+							onPress={() => {
+								props.send ? props.send : null;
+							}}
+						>
+							<Icon name="send" size={30} color="white" />
+						</TouchableOpacity>
+					</View>
+				</View>
+			</View>
+		</View>
+	);
+}
+
+const estilos = StyleSheet.create({
+	inputContainer: {
+		maxHeight: 141,
+		borderColor: '#000000',
+		flexDirection: 'row',
+		height: 50,
+		paddingBottom: 5,
+		paddingHorizontal: 5
+	},
+	inputChat: {
+		flex: 8,
+		maxHeight: 140,
+		fontSize: 17,
+		textAlignVertical: 'center',
+		paddingRight: 10,
+		paddingLeft: 15,
+		paddingVertical: 4
+	},
+	opciones: {
+		flex: 10,
+		height: '100%',
+		backgroundColor: 'white',
+		borderRadius: 20,
+		flexDirection: 'row',
+		justifyContent: 'center',
+		alignItems: 'center'
+	},
+	iconContainer: {
+		flexDirection: 'column-reverse',
+		height: '100%'
+	},
+	iconBox: {
+		flexDirection: 'row',
+		borderRadius: 20,
+		backgroundColor: '#458C8A',
+		marginLeft: 5
+	},
+	icon: {
+		padding: 5,
+		borderRadius: 20
+	}
+});
--- containers/Grupo.tsx
+++ containers/Grupo.tsx
@@ -1,17 +1,8 @@
 import * as React from 'react';
-import {
-	View,
-	StyleSheet,
-	TextInput,
-	ImageBackground,
-	TouchableOpacity,
-	FlatList,
-	TouchableNativeFeedback
-} from 'react-native';
-import Icon from 'react-native-vector-icons/MaterialIcons';
+import { View, StyleSheet, ImageBackground, FlatList } from 'react-native';
 import Header from '../components/HeaderLogin';
 import Mensaje, { Imensaje } from '../components/Mensajes';
-import * as DocumentPicker from 'expo-document-picker';
+import InputGrupo from '../components/InputGrupo';
 
 const colorFondo = '#e5ddd5';
 //e5ddd5 378C89
@@ -31,6 +22,16 @@ export default class Grupo extends React.Component<Iprops, Istate> {
 		super(props);
 		this.state = { height: 200, text: '' };
 	}
+
+	onContentSizeChange = (event) => {
+		this.setState({
+			height: event.nativeEvent.contentSize.height
+		});
+	};
+
+	onChangeText = (text) => {
+		this.setState({ text });
+	};
 
 	render() {
 		return (
@@ -54,70 +55,12 @@ export default class Grupo extends React.Component<Iprops, Istate> {
 								fecha="Fecha"
 							/>
 						</View>
-
-						<View
-							style={[
-								estilos.inputContainer,
-								{ height: Math.max(45, this.state.height) }
-							]}
-						>
-							<View style={estilos.opciones}>
-								<TextInput
-									placeholder="Escribe un mensaje"
-									multiline={true}
-									onChangeText={(text) => {
-										this.setState({ text });
-									}}
-									onContentSizeChange={(event) => {
-										this.setState({
-											height: event.nativeEvent.contentSize.height
-										});
-									}}
-									style={[
-										estilos.inputChat,
-										{ height: Math.max(45, this.state.height) }
-									]}
-									value={this.state.text}
-								/>
-							</View>
-
-							<View style={{ flexDirection: 'column-reverse' }}>
-								<View style={estilos.iconBox}>
-									{/**
-									 * Icon add
-									 */}
-
-									<View style={estilos.iconContainer}>
-										<TouchableOpacity
-											style={estilos.icon}
-											onPress={() => {
-												DocumentPicker.getDocumentAsync({
-													type: '*/*',
-													copyToCacheDirectory: false
-												})
-													.then((document) => {
-														alert(`uri: ${document.uri}
-													nombre: ${document.name}`);
-													})
-													.catch(() => alert('Algo salio mal'));
-											}}
-										>
-											<Icon name="add" size={30} color="white" />
-										</TouchableOpacity>
-									</View>
-
-									{/**
-									 * Icons send
-									 */}
-
-									<View style={estilos.iconContainer}>
-										<TouchableOpacity style={[estilos.icon]}>
-											<Icon name="send" size={30} color="white" />
-										</TouchableOpacity>
-									</View>
-								</View>
-							</View>
-						</View>
+						<InputGrupo
+							value={this.state.text}
+							onChangeText={this.onChangeText}
+							onContentSizeChange={this.onContentSizeChange}
+							height={this.state.height}
+						/>
 					</ImageBackground>
 				</View>
 			</View>
@@ -133,14 +76,6 @@ const estilos = StyleSheet.create({
 		paddingHorizontal: 10,
 		marginVertical: 10
 	},
-	inputContainer: {
-		maxHeight: 141,
-		borderColor: '#000000',
-		flexDirection: 'row',
-		height: 50,
-		paddingBottom: 5,
-		paddingHorizontal: 5
-	},
 	fondo: {
 		flex: 1,
 		resizeMode: 'cover',
@@ -150,37 +85,5 @@ const estilos = StyleSheet.create({
 		flex: 1,
 		resizeMode: 'cover',
 		justifyContent: 'center'
-	},
-	inputChat: {
-		flex: 8,
-		maxHeight: 140,
-		fontSize: 17,
-		textAlignVertical: 'center',
-		paddingRight: 10,
-		paddingLeft: 15,
-		paddingVertical: 4
-	},
-	opciones: {
-		flex: 10,
-		height: '100%',
-		backgroundColor: 'white',
-		borderRadius: 20,
-		flexDirection: 'row',
-		justifyContent: 'center',
-		alignItems: 'center'
-	},
-	iconContainer: {
-		flexDirection: 'column-reverse',
-		height: '100%'
-	},
-	iconBox: {
-		flexDirection: 'row',
-		borderRadius: 20,
-		backgroundColor: '#458C8A',
-		marginLeft: 5
-	},
-	icon: {
-		padding: 5,
-		borderRadius: 20
 	}
 });
